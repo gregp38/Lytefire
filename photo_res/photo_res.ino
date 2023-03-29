25beecfc9693e883d0937c1c2773627930516af0
gregp38 <gregpourcelot@hotmail.fr>
2023-03-29 20:16:59 +0200

    photoresistor comparaison done

--- photo_res/photo_res.ino
+++ photo_res/photo_res.ino
@@ -1,79 +1,35 @@
-// Inspire de https://wiki.dfrobot.com/Incremental_Photoelectric_Rotary_Encoder_-_400P_R_SKU__SEN0230#target_5
-/***************************************************
-  Two phase quadrature encoder(Incremental)
-* ****************************************************
-   To determine motor with encode （CW OR CCW）
+int motor=2;// déclaration du moteur sur le pin digital 2
+int sens1 = A0; // photoresistor 1 pin analog 1 mis en série avec résistance de 10kohm
+int sens2 = A1; //photoresistor 2 pin analog 2 mis en série avec résistance de 10kohm
+int tolerance = 20;// constante à définir pour ne pas déclencher le moteur sous ce seuil
+int val1, val2, diff;// définition de variables (photorésistance 1, photorésistance 2, différence entre les deux. 
 
-  @author Dong
-  @version  V1.0
-  @date  2016-5-26
-  All above must be included in any redistribution
-* ****************************************************/
+void setup() 
+{  
+  pinMode(sens1, INPUT);//la photorésistance1 envoie un signal analogique compris entre 0 et 1023
+  pinMode(sens2, INPUT);//la photorésistance2 envoie un signal analogique compris entre 0 et 1023
+  pinMode(motor, OUTPUT);//Signal envoyé au moteur
+  delay(2000); // a 2 secondes de délai 
+  Serial.begin(9600); // vitesse de communication
+}  
 
-//-----------------Encodeur chassis --------------------------//
-// Entrées
-#define  A_PHASE 2            // Phase A sur le pin n°2
-#define  B_PHASE 3            // Phase B sur le pin n°3
-// Sorties
-#define  MOTOR 13             // petit moteur test
-
-// Déclaration des variables
-int raf = 50;                 // Temps de raffraichissement (ms) du serial
-int pos = 0;                  // Position angulaire 1600/tour
-int oneturn = 1600;           // Un tour complet vaut valeur 1600 --> ! Dépend de l'encodeur rotatif choisi
-float degree = 0 ;            // La valeur sortie en degrés
-// Test moteur
-  // fourchette d'angle où le moteur ne s'allume pas  
-float min_degree = 0 ;        // La valeur sortie en degrés
-float max_degree = 120 ;      // La valeur sortie en degrés
-
-// 
-void setup() {
-  pinMode(A_PHASE, INPUT);    // Déclaration phase A en entrée 3
-  pinMode(B_PHASE, INPUT);    // Déclaration phase B en entrée 2
-  pinMode(MOTOR, OUTPUT);     // Déclaration phase B en entrée 2
-  Serial.begin(9600);         //Serial Port Baudrate: 9600 (vitesse de communication USB UART)
-  attachInterrupt(digitalPinToInterrupt( A_PHASE), interrupt_A, CHANGE); //Interrupt trigger mode: CHANGE
-  attachInterrupt(digitalPinToInterrupt( B_PHASE), interrupt_B, CHANGE); //Interrupt trigger mode: CHANGE
-}
-
-void loop() {
-  degree = to_degree(pos);
-
-  if ( (degree > min_degree) && (degree < max_degree) ) {
-    digitalWrite(MOTOR, HIGH);
-  } else {
-    digitalWrite(MOTOR, LOW);
-  }
-  Serial.println(degree);
-  delay(raf);     
-}
-
-void interrupt_A()    // Interrupt function
-{ char i; 
-  char j;
-  i = digitalRead( B_PHASE);
-  j = digitalRead( A_PHASE);
-  if (i ^ j ) {
-    pos ++;
-  } else {
-    pos--; 
-  }
-}
-
-void interrupt_B()  // Interrupt function
-{ char i; 
-  char j;
-  i = digitalRead( B_PHASE);
-  j = digitalRead( A_PHASE);
-  if (i ^ j ) {
-    pos --;
-  } else {
-    pos++; 
-  }  
-}
-
-float to_degree(float my_pos) // Change position en pulse/tour en degres
+ 
+void loop() 
 { 
-  return ((my_pos*360/oneturn) );
+  val1 = analogRead(sens1); // lire la valeur analogique en sortie de la photorésistance 1
+  val2 = analogRead(sens2); // lire la valeur analogique en sortie de la photorésistance 2
+  diff = val2-val1;// on calcule la différence
+  
+  Serial.println(diff);// on écrit la différence dans le moniteur série
+  
+  if(diff<-tolerance){           //Si la différence est en dessous du seuil
+    analogWrite(motor,255);      //alors faire tourner le moteur très vite
+  } else if (diff>tolerance) {   //Si la différence est au dessus du seuil
+    analogWrite(motor,150);      //alors faire tourner le moteur très lentement
+  } else {                       //Sinon entre -tolerance et +tolerance
+    analogWrite(motor,0);        //On éteind le moteur
+  }
+
+  delay(100);                    //On met un délai pour laisser le temps aux commandes
+
 }
